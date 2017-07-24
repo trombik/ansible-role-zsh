a2b1e622e67ec7e01725addd45628270936c0547
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2017-07-24 12:23:07 +0900

    [bugfix] support other platforms (#2)

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -2,7 +2,16 @@ require "spec_helper"
 require "serverspec"
 
 package = "zsh"
-additional_packages = ["zsh-navigation-tools"]
+additional_packages = []
+
+case os[:family]
+when "redhat"
+  additional_packages = ["zsh-html"]
+when "ubuntu"
+  additional_packages = ["zsh-lovers"]
+when "freebsd"
+  additional_packages = ["zsh-navigation-tools"]
+end
 
 describe package(package) do
   it { should be_installed }
